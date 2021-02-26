09d09966edb90c8f81e898dc7b645a5a9621f34c
David Ortinau <david.ortinau@microsoft.com>
2021-02-26 14:54:03 -0600

    first calculation

---
+++ src/SimpleCalculator/SimpleCalculator/SharedResources.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace SimpleCalculator
+{
+    public static class SharedResources
+    {
+        public static Color OpButtonBkColor
+        {
+            get
+            {
+                //return Color.White;
+                return Color.FromRgb(0xff, 0xa5, 0);
+            }
+        }
+    }
+}
+
---
+++ src/SimpleCalculator/SimpleCalculator/SimpleCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public static class Calculator
+    {
+        public static double Calculate(double value1, double value2, string mathOperator)
+        {
+            double result = 0;
+
+            switch (mathOperator)
+            {
+                case "÷":
+                    result = value1 / value2;
+                    break;
+                case "×":
+                    result = value1 * value2;
+                    break;
+                case "+":
+                    result = value1 + value2;
+                    break;
+                case "-":
+                    result = value1 - value2;
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
